2f40352c91c1246ce631018eac93c53b4836d46c
Alif Zakiansyah As Syauqi <alifzakiansyahassyauqi@gmail.com>
2022-05-21 19:27:50 +0700

    add madlibs

---
+++ madlibs/madlibs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+exclamation = input("Exclamation: ")
+adverb = input("Adverb: ")
+noun = input("Noun: ")
+adjective = input("Adjective: ")
+
+madlibs = f"{exclamation}! he said {adverb} as he jumped into his convertible {noun} \
+and drove off with his {adjective} wife."
+# Price, Roger; Leonard Stern (1974). The Original Mad Libs 1. Price Stern Sloan. p. 3. ISBN 978-0-8431-0055-6.
+
+print(madlibs)
